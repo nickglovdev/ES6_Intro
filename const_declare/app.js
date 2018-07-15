6cdff345baec725633c4ed914dfdaf7486c65b8e
Nicholas <jacksaintjack@gmail.com>
2018-07-15 14:18:03 -0500

    learned about when and how to use const

---
+++ const_declare/app.js
@@ -0,0 +1,38 @@
+// XXX 1. Old way of say not to reassign a variable
+
+// var VALUE = 'hello world';
+// VALUE ='foo bar';
+// console.log('value: ', VALUE);
+
+// XXX 2. With ES6
+
+// const value = 'hello world';
+// VALUE ='foo bar';
+// console.log('value: ', value);
+
+// XXX 3.
+
+// const VALUE = {};
+// VALUE.foo = 'bar';
+// console.log('value', VALUE)
+
+// XXX 4. ready only error
+
+// const VALUE = {};
+// VALUE = 'bar';
+// console.log('value', VALUE)
+
+// const adhere to the block scope
+
+// XXX will not work
+if(true){
+   const foo = 'bar';
+}
+console.log('foo', foo)
+
+//correct
+
+if(true){
+   const foo = 'bar';
+   console.log('foo', foo)
+}
